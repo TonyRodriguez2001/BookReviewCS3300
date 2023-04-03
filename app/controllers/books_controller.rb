a2ecb8124c9d60fb257e3fb9ffb2022ddf9e595d
Tony Rodriguez <trodrig4@uccs.edu>
2023-04-03 16:14:02 +0000

    Added an image attribute to model using active storage

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -65,6 +65,6 @@ class BooksController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def book_params
-      params.require(:book).permit(:title, :description, :rating, :published)
+      params.require(:book).permit(:title, :description, :rating, :published, :image)
     end
 end
